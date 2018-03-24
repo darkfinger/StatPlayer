fe61af08a975838abcd0b27d02eaca8d0e41f329
jrleriche <37277177+jrleriche@users.noreply.github.com>
2018-03-24 16:54:05 -0400

    classe equipe

--- StatPlayer/Equipe.cs
+++ StatPlayer/Equipe.cs
@@ -12,7 +12,92 @@ namespace StatPlayer
         string ville_;
         string division_;
         string conference_;
-        List<Joueur> joueurs_;
-        
+        List<Joueur> listJoueurs_;
+    
+        public string Nom
+        {
+            get
+            {
+                return nom_;
+
+            }
+             private set
+            {
+                nom_ = value;
+            }
+            
+        }
+
+        public string Ville
+        {
+            get
+            {
+                return ville_;
+            }
+
+            private set
+            {
+                ville_ = value;
+            }
+        }
+        public string Division
+        {
+            get
+            {
+                return division_;
+            }
+
+            private set
+            {
+                division_ = value;
+            }
+        }
+
+
+        public string Conference 
+        {
+            get
+            {
+                return conference_;
+            }
+
+            private set
+            {
+                conference_= value;
+            }
+        }
+
+        public List<Joueur> ListJoueurs
+
+        {
+            get
+
+            {
+                return listJoueurs_;
+            }
+
+            private set
+            {
+                listJoueurs_ = value;
+            } 
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 }
--- StatPlayer/Joueur.cs
+++ StatPlayer/Joueur.cs
@@ -8,12 +8,89 @@ namespace StatPlayer
 {
     class Joueur
     {
-        string nom_;
-        string equipe_;
-        Char[] position_;
-        int nbredeB_;
-        int nbrdeP_;
-        int nbrdeMa_;
-        
+        string nomJ_;
+        string nomEquip_;
+        Char position_;
+        uint nbrBut_;
+        uint nbrPass_;
+
+        public string NomJ
+        {
+            get
+            {
+                return nomJ_;
+            }
+
+            private set
+            {
+                nomJ_= value;
+            }
+        }
+
+
+
+        public string NomEquip
+        {
+            get
+            {
+                return nomEquip_;
+            }
+
+            private set
+            {
+                nomEquip_= value;
+            }
+        }
+
+        public char Position
+        {
+            get
+            {
+                return position_;
+            }
+
+            private set
+            {
+                position_= value;
+            }
+        }
+
+
+
+        public uint NbrBut
+        {
+            get
+            {
+                return nbrBut_;
+            }
+
+            private set
+            {
+                nbrBut_= value;
+            }
+        }
+
+
+
+
+        public uint NbrPass
+        {
+            get
+            {
+                return nbrPass_;
+            }
+
+            private set
+            {
+                nbrPass_= value;
+            }
+        }
+
+
+
+
     }
+
+
+
 }
